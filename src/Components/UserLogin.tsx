b263b2ec3ae43d920fa5dccc3fa5738b458b48ec
Timofey-Makhankov <timofey.makhankov@noseryoung.com>
2023-04-13 08:15:10 +0200

    added styles Folder & removed Logo in registry page

--- src/Components/UserLogin.tsx
+++ src/Components/UserLogin.tsx
@@ -1,29 +1,23 @@
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import '../Styles/ImageStyle.css'
 
 export default function UserLogin({ title, link, linkDescription }: { title: string, link: string, linkDescription: string }) {
     return (
-        <Grid container direction="column">
-            <Grid item xs={6}>
-                <Grid container direction="column" alignItems="center" justifyContent="center" spacing={3}>
-                    <Grid item xs={12}>
-                        <Typography variant="h3" component="h2" align='center' style={{ marginTop: '4em' }}>{title}</Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField id='email-input' label="email" variant='outlined' required onChange={() => { }} />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField id='password-input' label="password" variant='outlined' type="password" autoComplete="current-password" required onChange={() => { }} />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button variant='contained' onClick={() => { }}>{title}</Button>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Typography>{linkDescription}<Link href='/registration'>{link}</Link></Typography>
-                    </Grid>
-                </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="center" spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant="h3" component="h2" align='center' style={{ marginTop: '4em' }}>{title}</Typography>
             </Grid>
-            <Grid item xs={6} justifyContent="center">
-                <img src={require("../Images/logo.png")} alt={"Noser Young Logo"} />
+            <Grid item xs={12}>
+                <TextField id='email-input' label="email" variant='outlined' required onChange={() => { }} />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField id='password-input' label="password" variant='outlined' type="password" autoComplete="current-password" required onChange={() => { }} />
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant='contained' onClick={() => { }}>{title}</Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography>{linkDescription}<Link href='/registration'>{link}</Link></Typography>
             </Grid>
         </Grid>
     )
